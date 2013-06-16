d468463ba81587bf51457b7386195e4c8bea1cc5
atodi <atodi@twitter.com>
2013-06-15 20:38:33 -0700

    works

--- scroller/background.js
+++ scroller/background.js
@@ -1,7 +1,7 @@
-// Called when the user clicks on the browser action.
-
 chrome.browserAction.onClicked.addListener(automaticScroller);
-// chrome.browserAction.onClicked.addListener(onMessage);
+
+var currentX = 200
+var currentY = 200
 
 function automaticScroller(tab) {
     console.log('Scrolling ' + tab.url + '!');
@@ -15,26 +15,15 @@ function scrollToAPosition(x, y) {
     // add logic for whether to scroll or not
     document.body.style.backgroundColor="red";
     console.log("received x to be " + x + " and y to be " + y);
-    var run = "window.scrollTo(" + String(x) + "," + String(y) + ")";
+    // var run = "window.scrollTo(" + String(x) + "," + String(y) + ")";
+    var run = "window.scrollTo(" + String(currentX - 100) + "," + String(currentY - 100) + ")";
+    currentX = currentX - 100
+    currentY = currentY - 100
     console.log(run);
     chrome.tabs.executeScript({
         code: run
     });
 };
-
-// function automaticScroller(tab) {
-//     console.log('Turning ' + tab.url + ' red!');
-//     chrome.tabs.executeScript({
-//     // code: 'document.body.style.backgroundColor="read"'
-//         code: 'window.scrollTo(200, 200)'
-//     //  code: 'this.scrollToAPosition(0,0)'
-//       });
-//     //for (var k = 0; k < 500; k= k+100){
-//     //scrollToAPosition(k, 0);
-//     //}
-//     //scrollToAPosition(200, 200);
-// };
-
 
 function onMessage(evt) {
     console.log('received message ' + evt.data)
